f7497312ae7110e08f902bc2679bbf863f340651
Ya-Han Kuo <yu08723@gmail.com>
2015-03-13 23:33:00 +0800

    This is the non-recursive version

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -9,9 +9,27 @@
 	.type fibonacci, function
 
 fibonacci:
+	push {lr}
+
+	mov r4, #-1 @previous
+	mov r5, #1  @result
+        mov r6, #0  @i
+	mov r7, #0  @sum
+
+.L1:
+	add r7, r4, r5
+        mov r4, r5
+	mov r5, r7
+	add r6, r6, #1
+	cmp r6, r0     
+        ble .L1
+
+	mov r0,r5
+	pop {pc}
+
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, lr}
+@	push {r3, r4, r5, lr}
 
 	@ R4 = R0 - 0 (update flags)
 	@ if(R0 <= 0) goto .L3 (which returns 0)
@@ -28,16 +46,16 @@ fibonacci:
 
 	@ R0 = R5 + R0 (update flags)
 
-	pop {r3, r4, r5, pc}		@EPILOG
+@	pop {r3, r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
+@.L3:
+@	mov r0, #0			@ R0 = 0
+@	pop {r3, r4, r5, pc}		@ EPILOG
+@
+@.L4:
+@	mov r0, #1			@ R0 = 1
+@	pop {r3, r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
