675eee719415deeafd3f28c09de38568b1446459
Ya-Han Kuo <yu08723@gmail.com>
2015-03-13 23:30:59 +0800

    This is the recursive version

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -13,21 +13,33 @@ fibonacci:
 	@ PROLOG
 	push {r3, r4, r5, lr}
 
+	subs r4, r0, #0 
 	@ R4 = R0 - 0 (update flags)
+	ble .L3
 	@ if(R0 <= 0) goto .L3 (which returns 0)
+        
 
+	cmp r4, #1
 	@ Compare R4 wtih 1
+	beq .L4
 	@ If R4 == 1 goto .L4 (which returns 1)
 
+	add r0, r4, #4294967295
+	@subs r0, r4, #1
 	@ R0 = R4 - 1
+	bl fibonacci
 	@ Recursive call to fibonacci with R4 - 1 as parameter
 
+
+	mov r5, r0
 	@ R5 = R0
+	subs r0, r4, #2
 	@ R0 = R4 - 2
+	bl fibonacci
 	@ Recursive call to fibonacci with R4 - 2 as parameter
 
+	adds r0, r5, r0
 	@ R0 = R5 + R0 (update flags)
-
 	pop {r3, r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
